2dfe1037608aca5005f10fe2f0a8c7c46f12400a
Satenik Mnatsakanyan <satenik@leviathan.com>
2020-03-19 13:59:10 +0400

    Fixing the indentations

--- teacher.py
+++ teacher.py
@@ -4,17 +4,18 @@ def main():
     type = input("do what to start new exam (yes or no) ")
     if type == "yes":
         type = input(" do you want to find a slot or you have a date")
+        if type == "find":
+            print("found")
+            with open('jason.json') as file_data:
+                data = json.load(file_data)
+                for month in data ["month"]:
+                    if (month["filled"]) == "yes":
+                        print (month["day"])
 
-    if type == "find":
-        print("found")
-    with open('jason.json') as f:
-        data = json.load("f")
-    for month in data ["month"]:
-        if (month["filled"]) == "yes":
-            print (month["day"])
+        else:
+            if type == "date":
+                type == input ("please enter the day of the exam from 1-31")
 
-    if type == "date":
-        type == input ("please enter the day of the exam from 1-31")
-
-    if type == "no":
-        print("no")
+    else:
+        if type == "no":
+            print("no")
